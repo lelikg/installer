6d429a250461bc1948f110c447e6bef7d8c8324a
Leon Gordin <leon@innovid.com>
2013-11-14 22:39:43 +0200

    ubuntu prerequisites script fixing DWH deployment with empty root pass for mysql

--- installer/Installer.class.php
+++ installer/Installer.class.php
@@ -954,11 +954,13 @@ class Installer
 		$arguments = sprintf('-h %s -P %s -u %s -d %s', AppConfig::get(AppConfigAttribute::DB1_HOST), AppConfig::get(AppConfigAttribute::DB1_PORT), AppConfig::get(AppConfigAttribute::DB_ROOT_USER), AppConfig::get(AppConfigAttribute::DWH_DIR));
 		if (AppConfig::get(AppConfigAttribute::DB_ROOT_PASS))
 			$arguments .= ' -p ' . AppConfig::get(AppConfigAttribute::DB_ROOT_PASS);
-			
+        else // script fails with empty pass, instead add "-p ''"
+            $arguments .= " -p ''";
+
 		if(AppConfig::get(AppConfigAttribute::DB1_CREATE_NEW_DB))
 		{
 			Logger::logMessage(Logger::LEVEL_INFO, "Creating data warehouse");
-			$cmd = sprintf("%s/setup/dwh_setup.sh $arguments", AppConfig::get(AppConfigAttribute::DWH_DIR));	
+			$cmd = sprintf("%s/setup/dwh_setup.sh $arguments", AppConfig::get(AppConfigAttribute::DWH_DIR));
 			if (!OsUtils::execute($cmd)){
 				return "Failed running data warehouse initialization script";
 			}
